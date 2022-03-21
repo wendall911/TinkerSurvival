07e512fb00f9d4b9c4db9d1f168aea9470e2d453
Wendall Cada <wendallc@83864.com>
2022-03-20 19:38:26 -0700

    Moving more files into generated resources.

--- src/main/java/tinkersurvival/data/ModBlockTagsProvider.java
+++ src/main/java/tinkersurvival/data/ModBlockTagsProvider.java
@@ -27,12 +27,41 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
     protected void addTags() {
         getBuilder(TagManager.Blocks.ALWAYS_BREAKABLE)
             .add(Blocks.GRAVEL)
+            .addTag(Tags.Blocks.GRAVEL)
 			.addTag(BlockTags.LEAVES)
             .addTag(BlockTags.DIRT)
-            .add(Blocks.GRASS_BLOCK)
-            .add(Blocks.PODZOL)
-            .add(Blocks.COARSE_DIRT)
+			.addTag(Tags.Blocks.SAND)
             .addTag(BlockTags.SAND);
+
+        getBuilder(TagManager.Blocks.ALWAYS_DROPS)
+            .add(Blocks.GRAVEL)
+            .addTag(Tags.Blocks.GRAVEL)
+			.addTag(BlockTags.LEAVES)
+            .addTag(BlockTags.DIRT)
+			.addTag(Tags.Blocks.SAND)
+            .addTag(BlockTags.SAND);
+
+        getBuilder(TagManager.Blocks.LOOSE_ROCK_PLACEABLE_ON)
+            .add(Blocks.GRAVEL)
+            .addTag(Tags.Blocks.GRAVEL)
+			.add(Blocks.STONE)
+			.add(Blocks.CALCITE)
+			.add(Blocks.GRANITE)
+			.add(Blocks.DIORITE)
+			.add(Blocks.ANDESITE)
+			.add(Blocks.COAL_ORE)
+			.addTag(Tags.Blocks.ORES_COAL)
+			.add(Blocks.SANDSTONE)
+			.add(Blocks.IRON_BLOCK)
+			.addTag(Tags.Blocks.ORES_IRON)
+            .add(Blocks.COPPER_ORE)
+			.addTag(Tags.Blocks.ORES_COPPER)
+			.add(Blocks.MOSSY_COBBLESTONE)
+			.add(Blocks.RED_SANDSTONE)
+            .addTag(BlockTags.DIRT)
+            .addTag(BlockTags.SAND)
+			.addTag(Tags.Blocks.SAND)
+			.addTag(BlockTags.TERRACOTTA);
     }
 
     private void builder(Tag.Named<Block> tag, IBlockProvider... items) {
