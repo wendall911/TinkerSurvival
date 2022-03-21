a327193c9ff84f19082544c7c8a5bd4e440c1fc5
Wendall Cada <wendallc@83864.com>
2022-03-20 19:38:26 -0700

    Add normal drops for grass_fiber

--- src/main/java/tinkersurvival/loot/TinkerSurvivalLootTables.java
+++ src/main/java/tinkersurvival/loot/TinkerSurvivalLootTables.java
@@ -1,14 +1,29 @@
 package tinkersurvival.loot;
 
-/*
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.LootTableList;
-*/
+import com.google.gson.JsonObject;
+
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
+import net.minecraftforge.common.loot.LootModifier;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
 import tinkersurvival.TinkerSurvival;
+import tinkersurvival.world.TinkerSurvivalWorld;
 
+@EventBusSubscriber(modid = TinkerSurvival.MODID, bus = Bus.MOD)
 public class TinkerSurvivalLootTables {
-    
+
     /*
     public static ResourceLocation TREASURE;
 
@@ -20,5 +35,41 @@ public class TinkerSurvivalLootTables {
         return LootTableList.register(new ResourceLocation(TinkerSurvival.MODID + ":" + path));
     }
     */
+
+	@SubscribeEvent
+	public static void registerModifiers(RegistryEvent.Register<GlobalLootModifierSerializer<?>> event) {
+		event.getRegistry().register(
+            new LootTableSerializer().setRegistryName(TinkerSurvival.MODID, "grass_fiber_drops")
+		);
+	}
+
+	public static class LootTableSerializer extends GlobalLootModifierSerializer<LootTableModifier> {
+
+		@Override
+		public LootTableModifier read(ResourceLocation location, JsonObject object, LootItemCondition[] ailootcondition) {
+			return new LootTableModifier(ailootcondition);
+		}
+
+		@Override
+		public JsonObject write(LootTableModifier instance) {
+			return new JsonObject();
+		}
+
+	}
+
+	private static class LootTableModifier extends LootModifier {
+
+		protected LootTableModifier(LootItemCondition[] conditionsIn) {
+			super(conditionsIn);
+		}
+
+		@Nonnull
+		@Override
+		protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
+			generatedLoot.add(new ItemStack(TinkerSurvivalWorld.GRASS_FIBER.get()));
+			return generatedLoot;
+		}
+
+	}
 
 }
