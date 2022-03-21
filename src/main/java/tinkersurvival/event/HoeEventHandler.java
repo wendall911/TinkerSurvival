3fc16d539f6cac65d1803a91b9927e4c9898b04b
Wendall Cada <wendallc@83864.com>
2022-03-20 19:38:26 -0700

    Disable non-whitelisted hoe tools.

--- src/main/java/tinkersurvival/event/HoeEventHandler.java
+++ src/main/java/tinkersurvival/event/HoeEventHandler.java
@@ -1,35 +1,34 @@
 package tinkersurvival.event;
 
-/*
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
-import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.event.entity.player.UseHoeEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 import tinkersurvival.client.sound.Sounds;
+import tinkersurvival.TinkerSurvival;
 import tinkersurvival.util.ItemUse;
-*/
 
+@Mod.EventBusSubscriber(modid = TinkerSurvival.MODID)
 public class HoeEventHandler {
 
-    /*
     @SubscribeEvent
-    public void onHoeBlock(UseHoeEvent event) {
-        EntityPlayer player = event.getEntityPlayer();
+    public static void onHoeBlock(UseHoeEvent event) {
+        final Player player = event.getPlayer();
 
-        if (player == null
-            || player instanceof FakePlayer
-            || player.capabilities.isCreativeMode
-                ) {
-            return;
-        }
+        if (!player.isCreative()) {
+            final ItemStack handStack = player.getMainHandItem();
+            final Level level = player.getLevel();
 
-        if (!ItemUse.isWhitelistItem(player.getHeldItemMainhand())) {
-            Sounds.play(player, Sounds.HOE_FAIL, 0.2F, 1.0F);
-            event.setCanceled(true);
+            if (!ItemUse.isWhitelistItem(handStack)) {
+                level.playSound(null, player.getOnPos(), Sounds.HOE_FAIL.get(), SoundSource.BLOCKS, 0.2F, 1.0F);
+                event.setCanceled(true);
+            }
         }
     }
-    */
 
 }
