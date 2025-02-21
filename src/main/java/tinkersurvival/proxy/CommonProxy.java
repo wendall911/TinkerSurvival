e106cfe0c73f1caa227615ccbadcdccf02917771
Wendall Cada <wendallc@83864.com>
2025-02-21 12:21:23 -0800

    Add TCon items to proper categories. Apply variant rendering for JEI/Creative Tabs.

--- src/main/java/tinkersurvival/proxy/CommonProxy.java
+++ src/main/java/tinkersurvival/proxy/CommonProxy.java
@@ -1,9 +1,14 @@
 package tinkersurvival.proxy;
 
-import net.minecraft.core.registries.Registries;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
+
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -12,6 +17,12 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.RegisterEvent;
+
+import slimeknights.tconstruct.common.registration.CastItemObject;
+import slimeknights.tconstruct.library.tools.helper.ToolBuildHandler;
+import slimeknights.tconstruct.smeltery.TinkerSmeltery;
+import slimeknights.tconstruct.tools.TinkerToolParts;
+import slimeknights.tconstruct.tools.TinkerTools;
 
 import tinkersurvival.common.TinkerSurvivalModule;
 import tinkersurvival.items.TConItems;
@@ -57,14 +68,30 @@ public class CommonProxy {
 
         @SubscribeEvent(priority = EventPriority.LOWEST)
         public static void registerCreativeTab(BuildCreativeModeTabContentsEvent event) {
+            Consumer<ItemStack> output = ((CreativeModeTab.Output) event)::accept;
+
+            if (event.getTabKey() == TinkerTools.tabTools.getKey()) {
+                ToolBuildHandler.addVariants(output, TConItems.KNIFE.get(), "");
+                ToolBuildHandler.addVariants(output, TConItems.SAW.get(), "");
+            }
+
+            if (event.getTabKey() == TinkerToolParts.tabToolParts.getKey()) {
+                TConItems.SAW_BLADE.get().addVariants(output, "");
+            }
+
+            if (event.getTabKey() == TinkerSmeltery.tabSmeltery.getKey()) {
+                addCast(event, CastItemObject::get);
+                addCast(event, CastItemObject::getSand);
+                addCast(event, CastItemObject::getRedSand);
+            }
+
             if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
-                event.accept(new ItemStack(TConItems.KNIFE.get()));
-                event.accept(new ItemStack(TConItems.SAW.get()));
-                event.accept(new ItemStack(TConItems.SAW_BLADE.get()));
-                event.accept(new ItemStack(TConItems.SAW_BLADE_CAST.get()));
-                event.accept(new ItemStack(TConItems.SAW_BLADE_CAST.get()));
                 event.accept(new ItemStack(TinkerSurvivalItems.MODPACK_BOOK));
             }
+        }
+
+        private static void addCast(CreativeModeTab.Output output, Function<CastItemObject, ItemLike> getter) {
+            output.accept(getter.apply(TConItems.SAW_BLADE_CAST));
         }
 
     }
